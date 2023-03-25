9385ef8f6cf83cd9f317e4a0fdbe807525b48515
Federico Barbieri <federicobarbieri54@gmail.com>
2023-03-25 17:43:46 +0100

    v1

---
+++ script.js
@@ -0,0 +1,9 @@
+import {animate, timeline} from "https://cdn.skypack.dev/motion";
+
+
+  const sequence = [
+    [".header_graphics", {y: [0, -100, 0]}, { duration: 1 }],
+    [".dark_button", {x: [0, -100, 0]}, { duration: 1 }],
+  ]
+
+  timeline(sequence, {repeat: 1});
